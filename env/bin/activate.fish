d5a0cfbbafdbc69cfbf6e614bf1491939deadf3f
weilbach <weilbach@umich.edu>
2019-09-17 15:45:58 -0700

    committing new environment stuff

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/justinweilbach/Desktop/justin_website/env"
+set -gx VIRTUAL_ENV "/Users/justinweilbach/Desktop/website/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
